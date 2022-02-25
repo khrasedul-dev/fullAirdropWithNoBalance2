470348c238f423cd62d71bc25e4b93ae3490ff97
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-02-25 23:15:32 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,413 @@
+const {
+	Telegraf,
+	Composer,
+	Stage,
+	session,
+	BaseScene,
+	WizardScene
+} = require('micro-bot')
+const mongoose = require('mongoose')
+
+const userModel = require('./userModel')
+const checkGroup = require('./checkGroup')
+
+
+const bot = new Telegraf('5199112586:AAH-1XFcsHlB3DOwQCFa0ZZP8g0Mys3g_Gs')
+
+// const bot = new Composer()
+
+
+mongoose.connect('mongodb+srv://rasedul20:rasedul20@cluster0.hzttg.mongodb.net/telegramProject?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}).catch((e) => {
+	console.log(e)
+}).then((d) => console.log('Database connected')).catch((e) => console.log(e))
+
+
+bot.use(session({
+	property: 'user',
+	getSessionKey: (ctx) => ctx.chat && ctx.chat.id,
+}))
+
+
+bot.action('join', ctx => {
+
+	ctx.telegram.sendMessage(ctx.chat.id, `Task 1: \n\nPlease Join our telegram gorup (If you are already a member of our group send the word "Airdrop" as a messsage in the group, then return here and click done to continue) \n\nhttps://t.me/cryptoteddies \n\nClick done to proceed after you have joined`, {
+		reply_markup: {
+			inline_keyboard: [
+				[{
+					text: "Done",
+					callback_data: "groupJoin"
+				}]
+			]
+		}
+	}).catch((e) => console.log(" Something is wrong"))
+})
+
+bot.action('groupJoin', (ctx) => {
+
+	const data = checkGroup.find({
+		userId: ctx.from.id
+	})
+
+	data.then((data) => {
+
+		if (data.length > 0) {
+
+
+			ctx.telegram.sendMessage(ctx.chat.id, `Task 2: \n\nPlease Download Beta https://www.cryptoteddies.com/download-beta/ \n\nClick done to next task`, {
+				reply_markup: {
+					inline_keyboard: [
+						[{
+							text: "Done",
+							callback_data: "channelJoin"
+						}]
+					]
+				}
+			}).catch((e) => console.log(" Something is wrong"))
+
+		} else {
+
+
+			ctx.telegram.sendMessage(ctx.chat.id, `Task 1: \n\nPlease Join our telegram gorup (If you are already a member of our group send the word "Airdrop" as a messsage in the group, then return here and click done to continue) \n\nhttps://t.me/cryptoteddies \n\nClick done to proceed after you have joined`, {
+				reply_markup: {
+					inline_keyboard: [
+						[{
+							text: "Done",
+							callback_data: "groupJoin"
+						}]
+					]
+				}
+			}).catch((e) => console.log(" Something is wrong"))
+		}
+
+	}).catch((e) => console.log("Something is wrong"))
+
+
+})
+
+const input_form = new WizardScene('input_data',
+	(ctx) => {
+
+
+		ctx.user.userId = ctx.from.id
+		ctx.user.userName = ctx.from.first_name
+
+		ctx.reply(`Task 3: \n\nA. Click the link: \nhttps://www.Twitter.com/cryptoteddies \n\nFollow us on Twitter \nB. Like one of our Twitter posts, make a Twitter comment and retweet our post  \n\nNote: you must retweet our post, not some other person's post \n\nWhen you are done, return here and enter your Twitter username to proceed. \n\nOur team will manually verify if you have completed this task`).catch((e) => console.log(" Something is wrong"))
+		return ctx.wizard.next()
+	},
+	(ctx) => {
+
+		ctx.user.twitter = ctx.update.message.text
+
+		ctx.reply(`Task 4: \n\nA. Click the link:\nhttps://www.instagram.com/invites/contact/?i=djuf9mqsvr0a&utm_content=nd6pggz \n\nFollow us on instagram, Comment and share one of our posts there. \n\nNote you must comment and share our post, not some other person's post \n\nWhen you are done, return here and enter your instagram username to proceed. \n\nOur team will manually verify if you have completed this task`).catch((e) => console.log(" Something is wrong"))
+		return ctx.wizard.next()
+	},
+	(ctx) => {
+
+		ctx.user.instagram = ctx.update.message.text
+
+		ctx.reply(`Task 5: \n\nAdd Your Enjin Collectible wallet address`).catch((e) => console.log(" Something is wrong"))
+		return ctx.wizard.next()
+	},
+	(ctx) => {
+
+		ctx.user.ejin = ctx.update.message.text
+
+		ctx.reply(`Task 6: \n\nAdd Your Crypto Teddies Username`).catch(() => console.log(" Something is wrong"))
+		return ctx.wizard.next()
+	},
+	(ctx) => {
+
+		const data = userModel.find({
+			userId: ctx.from.id
+		})
+
+		data.then((data) => {
+
+
+			if (data.length > 0) {
+
+				const ref_id = parseInt(data[0].referrer_id)
+
+				const inputData = {
+					twitter: ctx.user.twitter,
+					instagram: ctx.user.instagram,
+					ejin: ctx.user.ejin,
+					teddies_username: ctx.update.message.text
+				}
+
+
+				const data2 = userModel.updateOne({
+					userId: ctx.from.id
+				}, inputData)
+
+				data2.then((data) => {
+
+
+					const data3 = userModel.find({
+						userId: ref_id
+					})
+
+					data3.then((data) => {
+
+
+						const ref_count = parseInt(data[0].referral_count)
+
+
+						const update_ref = {
+							referral_count: ref_count + 1
+						}
+
+
+						const data4 = userModel.updateOne({
+							userId: ref_id
+						}, update_ref)
+
+						data4.then((data) => {
+
+							ctx.telegram.sendMessage(ctx.from.id, "Account Info:\n\nName - " + ctx.from.first_name + "\nWallet Address - " + ctx.update.message.text + "\nReferral Users - 0 \n\nReferral Link -\n\n (Tap to copy your link) \n\n`https://t.me/" + ctx.botInfo.username + "?start=" + ctx.from.id + "`\n\nShare your referral links with your friends on Telegram, WhatsApp, Facebook, and Twitter and tell them about this airdrop. When they join this contest through your referral link, your referral Users count . We will award 0.5bnb worth of tokens each to 150 persons with the highest number of referrals. Good luck", {
+								reply_markup: {
+									inline_keyboard: [
+										[{
+											text: "Refresh",
+											callback_data: "start"
+										}]
+									]
+								},
+								parse_mode: "Markdown"
+							}).catch((e) => console.log(" Something is wrong"))
+
+
+						}).catch((e) => console.log("Something is wrong"))
+
+					})
+
+
+				}).catch((e) => console.log("Something is wrong"))
+
+
+			} else {
+
+				console.log(ctx.user)
+
+				const inputData = new userModel({
+					userId: ctx.from.id,
+					name: ctx.from.first_name,
+					twitter: ctx.user.twitter,
+					instagram: ctx.user.instagram,
+					ejin: ctx.user.ejin,
+					teddies_username: ctx.update.message.text,
+					referral_count: '0'
+				})
+
+				inputData.save((e) => {
+
+					if (e) {
+						throw e
+					} else {
+
+						ctx.telegram.sendMessage(ctx.from.id, "Account Info:\n\nName - " + ctx.from.first_name + "\nWallet Address - " + ctx.update.message.text + "\nReferral Users - 0 \n\nReferral Link - \n\n (Tap to copy your link) \n\n `https://t.me/" + ctx.botInfo.username + "?start=" + ctx.from.id + "`\n\nShare your referral links with your friends on Telegram, WhatsApp, Facebook, and Twitter and tell them about this airdrop. When they join this contest through your referral link, your referral Users count . We will award 0.5bnb worth of tokens each to 150 persons with the highest number of referrals. Good luck", {
+							reply_markup: {
+								inline_keyboard: [
+									[{
+										text: "Refresh",
+										callback_data: "start"
+									}]
+								]
+							},
+							parse_mode: "Markdown"
+						}).catch((e) => console.log(" Something is wrong"))
+
+
+					}
+				})
+			}
+
+		}).catch((e) => console.log("Something is wrong"))
+
+
+		return ctx.scene.leave()
+	}
+)
+
+
+const stage = new Stage([input_form], {
+	sessionName: 'user'
+})
+
+bot.use(stage.middleware())
+
+
+bot.action('channelJoin', Stage.enter('input_data'))
+
+
+bot.start((ctx) => {
+
+	const ref = ctx.startPayload
+
+	const data = userModel.find({
+		userId: ctx.from.id
+	})
+
+
+	data.then((data) => {
+
+		if (data.length > 0) {
+
+			const wallet = data[0].wallet
+			const r = data[0].referral_count
+
+
+			ctx.telegram.sendMessage(ctx.from.id, "Account Info:\n\nName - " + ctx.from.first_name + "\nWallet Address - " + wallet + "\nReferral Users - " + r + " \n\nReferral Link - \n\n (Tap to copy your link) \n\n **`https://t.me/" + ctx.botInfo.username + "?start=" + ctx.from.id + "`**\n\nShare your referral links with your friends on Telegram, WhatsApp, Facebook, and Twitter and tell them about this airdrop. When they join this contest through your referral link, your referral Users count . We will award 0.5 bnb worth of tokens each to 150 persons with the highest number of referrals. Good luck", {
+				reply_markup: {
+					inline_keyboard: [
+						[{
+							text: "Refresh",
+							callback_data: "start"
+						}]
+					]
+				},
+				parse_mode: "Markdown"
+			}).catch((e) => console.log(" Something is wrong"))
+
+
+		} else {
+
+
+			if (ref.length > 0) {
+
+				const inputData = new userModel({
+					userId: ctx.from.id,
+					name: ctx.from.first_name,
+					referrer_id: ref,
+					referral_count: 0
+				})
+
+				const data = inputData.save()
+
+				data.then((data) => {
+
+
+					ctx.telegram.sendMessage(ctx.chat.id, `Hello ${ctx.from.first_name}, \nWelcome to Dogymon Airdrop Contest.\n\nWe will be giving 0.5BNB worth of dogymon tokens each to 150 winners who have completed our simple airdrop tax and had the most number of referrals. \n\nClick the start button below to join the contest.`, {
+						reply_markup: {
+							inline_keyboard: [
+								[{
+									text: "Start",
+									callback_data: "join"
+								}]
+							]
+						}
+					}).catch((e) => console.log(" Something is wrong"))
+
+
+				}).catch((e) => console.log("Something is wrong "))
+
+
+			} else {
+
+				ctx.telegram.sendMessage(ctx.chat.id, `Hello ${ctx.from.first_name}, \nWelcome to Dogymon Airdrop Contest.\n\nWe will be giving 0.5BNB worth of dogymon tokens each to 150 winners who have completed our simple airdrop tax and had the most number of referrals. \n\nClick the start button below to join the contest.`, {
+					reply_markup: {
+						inline_keyboard: [
+							[{
+								text: "Start",
+								callback_data: "join"
+							}]
+						]
+					}
+				}).catch((e) => console.log(" Something is wrong"))
+			}
+
+		}
+
+
+	}).catch((e) => ctx.reply("Please try again"))
+
+})
+
+
+bot.action("start", (ctx) => {
+
+	const data = userModel.find({
+		userId: ctx.from.id
+	})
+	data.then((data) => {
+		const wallet = data[0].wallet
+		const r = data[0].referral_count
+
+
+		ctx.telegram.sendMessage(ctx.from.id, "Account Info:\n\nName - " + ctx.from.first_name + "\nWallet Address - " + wallet + "\nReferral Users - " + r + " \n\nReferral Link -\n\n (Tap to copy your link) \n\n **`https://t.me/" + ctx.botInfo.username + "?start=" + ctx.from.id + "`**\n\nShare your referral links with your friends on Telegram, WhatsApp, Facebook, and Twitter and tell them about this airdrop. When they join this contest through your referral link, your referral Users count . We will award 0.5bnb worth of tokens each to 150 persons with the highest number of referrals. Good luck", {
+			reply_markup: {
+				inline_keyboard: [
+					[{
+						text: "Refresh",
+						callback_data: "start"
+					}]
+				]
+			},
+			parse_mode: "Markdown"
+		}).catch((e) => console.log(" Something is wrong"))
+	}).catch((e) => ctx.reply("Please try with /start"))
+})
+
+
+bot.on('new_chat_members', (ctx) => {
+
+	const data = checkGroup.find({
+		userId: ctx.from.id
+	})
+
+	data.then((data) => {
+
+		if (data.length > 0) {
+			console.log("User Already Added")
+		} else {
+
+			const data = new checkGroup({
+				userId: ctx.from.id
+			})
+			const d = data.save()
+			d.catch((e) => console.log("Something is wrong"))
+		}
+
+	}).catch((e) => console.log("Something is wrong"))
+
+})
+
+
+bot.on('text', (ctx) => {
+
+	const message = ctx.update.message.text
+
+	const r = /Airdrop/gi
+
+	if (message.match(r)) {
+
+		const data = checkGroup.find({
+			userId: ctx.from.id
+		})
+
+		data.then((data) => {
+
+			if (data.length > 0) {
+				console.log("User Already Added")
+			} else {
+
+				const data = new checkGroup({
+					userId: ctx.from.id
+				})
+				const d = data.save()
+				d.catch((e) => console.log("Something is wrong"))
+			}
+
+		}).catch((e) => console.log("Something is wrong"))
+
+	}
+
+})
+
+
+bot.launch()
+
+// module.exports = bot
---
+++ userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    userId : {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    referrer_id: {
+        type: String
+    },
+    referral_count: {
+        type: String
+    },
+    twitter: {
+        type: String
+    },
+    instagram: {
+        type: String
+    },
+    ejin: {
+        type: String
+    },
+    teddies_username: {
+        type: String
+    }
+},
+{versionKey: false}
+
+)
+
+module.exports = mongoose.model('user',userSchema)
